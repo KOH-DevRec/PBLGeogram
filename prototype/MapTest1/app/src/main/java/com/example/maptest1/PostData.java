195ec064f75febab9517d0bcea7f19630f7b4ca0
KOH1226 <g231s026@s.iwate-pu.ac.jp>
2021-01-07 10:28:04 +0900

    firebase関連を追加

---
+++ prototype/MapTest1/app/src/main/java/com/example/maptest1/PostData.java
@@ -0,0 +1,51 @@
+package com.example.maptest1;
+
+import com.google.firebase.firestore.ServerTimestamp;
+import com.google.type.LatLng;
+
+import java.util.Date;
+
+//投稿(スレッド)のデータ構造
+public class PostData {
+    private String name;         //スレッド名
+    private String tag;          //タグ(型は変更する可能性有)
+    private String caption;      //説明文
+    private String firebaseKey;  //IDの代用
+    public Date timestamp;       //日時
+    private LatLng position;     //投稿位置
+    public int good;             //いいね数
+
+    public PostData(){}
+
+    //データ送信用(postformで利用)
+    public PostData(String key, String name, String tag, String cap, Date date){
+        this.firebaseKey = key;
+        this.name = name;
+        this.tag = tag;
+        this.caption = cap;
+        this.timestamp = date;
+        //this.position = pos;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getTag() { return tag; }
+    public void setTag(String tag) { this.tag = tag; }
+
+    public String getCaption() { return caption; }
+    public void setCaption(String caption) { this.caption = caption; }
+
+    public String getFirebaseKey() { return firebaseKey; }
+    public void setFirebaseKey(String firebaseKey) { this.firebaseKey = firebaseKey; }
+
+    @ServerTimestamp
+    public Date getTimestamp() { return timestamp; }
+    public void setTimestamp(Date timestamp) { this.timestamp = timestamp; }
+
+    public int getGood() { return good; }
+    public void setGood(int good) { this.good = good; }
+
+    public LatLng getPosition() { return position; }
+    public void setPosition(LatLng pos) { this.position = pos; }
+}
--- prototype/MapTest1/app/src/main/java/com/example/maptest1/PostformActivity.java
+++ prototype/MapTest1/app/src/main/java/com/example/maptest1/PostformActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.Date;
+
 public class PostformActivity extends AppCompatActivity{
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference();
+    EditText nameEditText, captionEditText, tagEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,7 @@ public class PostformActivity extends AppCompatActivity{
         ImageButton UButton = findViewById(R.id.buser);
         ImageButton SButton = findViewById(R.id.bgralss);
         ImageButton NButton = findViewById(R.id.bbell);
+        ImageButton PButton = findViewById(R.id.btoko);
 
         MButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +61,34 @@ public class PostformActivity extends AppCompatActivity{
             }
         });
 
+        PButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nameEditText = (EditText) findViewById(R.id.edit_text1);
+        captionEditText = (EditText) findViewById(R.id.edit_text2);
+        tagEditText = (EditText) findViewById(R.id.edit_text3);
+    }
+
+    public void post(View v) {
+        String name = nameEditText.getText().toString();
+        String caption = captionEditText.getText().toString();
+        String tag = tagEditText.getText().toString();
+        Date date = new Date();
+        String key = reference.push().getKey();
+
+//    引数のToDoDataの内容をデータベースに送る。
+        PostData postData = new PostData(key, name, caption, tag, date);
+
+        reference.child(key).setValue(postData).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void v) {
+                finish();
+            }
+        });
     }
 }
