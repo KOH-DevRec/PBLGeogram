e71b0033681a528d1624ec5aab13d5dcbf204513
KOH1226 <g231s026@s.iwate-pu.ac.jp>
2021-01-27 03:48:51 +0900

    追加：DB内投稿データ閲覧レイアウト、閲覧画面用アイコン

---
+++ prototype/MapTest1/app/src/main/java/com/example/maptest1/PostViewActivity.java
@@ -0,0 +1,54 @@
+package com.example.maptest1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PostViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postview);
+
+        ImageButton MButton = findViewById(R.id.bmap);
+        ImageButton UButton = findViewById(R.id.buser);
+        ImageButton SButton = findViewById(R.id.bgralss);
+        ImageButton NButton = findViewById(R.id.bbell);
+
+        MButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        UButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), UserActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        SButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SearchlistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        NButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), NoticeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
